77bbe053e6b4a6c919c05dfb4af2f044836269ea
NaoOtosaka <1049798454@qq.com>
2024-09-03 14:27:04 +0800

    feat.追加词库插件

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
-
-
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
 nonebot.load_builtin_plugins('echo')
-
 
 nonebot.load_from_toml("pyproject.toml")
 
